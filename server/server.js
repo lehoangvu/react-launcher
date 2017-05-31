bc3794e27e8b7d6ef80939cbb37b61821bf1c19d
lehoangvu <hoangvu171819@gmail.com>
2017-05-31 20:07:36 +0700

    add build

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ var express = require('express');
 var app = express();
 var request = require("request");
 
+app.use((req, res, next) => {
+  res.header("cache-control", "max-age=604800");
+  res.header("content-encoding", "gzip");
+  next();
+});
+
 app.use(
     "/", //the URL throught which you want to access to you static content
     express.static(__dirname + '/../') //where your static content is located in your filesystem
