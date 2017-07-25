77eb4c077dea6305a2bed690ec447fbfb305613b
Vũ Lê <vu.le@tiki.vn>
2017-07-25 14:42:17 +0700

    add config mongo prod

--- server/server-render-origin.js
+++ server/server-render-origin.js
@@ -32,14 +32,14 @@ app.use((req,res,next) => {
    next(); 
 });
 
-app.use(compression({filter: (req, res) => {
-  if (req.headers['x-no-compression']) {
-    // don't compress responses with this request header
-    return false
-  }
-  // fallback to standard filter function
-  return compression.filter(req, res);
-}}));
+// app.use(compression({filter: (req, res) => {
+//   if (req.headers['x-no-compression']) {
+//     // don't compress responses with this request header
+//     return false
+//   }
+//   // fallback to standard filter function
+//   return compression.filter(req, res);
+// }}));
 
 app.use('/public', Express.static('./public'))
 app.use('/dist', Express.static('./dist'))
